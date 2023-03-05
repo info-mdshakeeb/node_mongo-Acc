04ad116e199e1f03f7b5205936f68c0efd5ea7df
itzshakeeb <itzshakeeb@gmail.com>
2023-03-05 23:36:24 +0600

    make my first server

---
+++ M2_Introduction To Node/index.js
@@ -0,0 +1,12 @@
+const { add, reduce } = require("./other.js");
+const http = require("http")
+const sum = add(10, 20)
+const sub = reduce(30, 20)
+console.log();
+
+const server = http.createServer((req, res) => {
+    res.end("thats my server :(")
+})
+const port = 3210;
+server.listen(port, console.log(`server is running port - ${port}`))
+
---
+++ M2_Introduction To Node/other.js
@@ -0,0 +1,4 @@
+const name = "shakeeb";
+const add = (a, b) => a + b;
+const reduce = (a, b) => a - b;
+module.exports = { add, reduce }
